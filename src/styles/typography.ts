3d7867a27a815139424881b870726adc4e70bc99
DaeHyun Kwon <104804antonio@gmail.com>
2025-01-23 23:12:09 +0900

    feat: 타이포그래피 설정

---
+++ src/styles/typography.ts
@@ -0,0 +1,48 @@
+import {StyleSheet} from 'react-native';
+
+const FONT_FAMILY = 'pretendard';
+
+export const typography = StyleSheet.create({
+  title: {
+    fontFamily: FONT_FAMILY,
+    fontSize: 24,
+    fontWeight: '600',
+    letterSpacing: -0.0552,
+    lineHeight: 32,
+  },
+  head: {
+    fontFamily: FONT_FAMILY,
+    fontSize: 20,
+    fontWeight: '600',
+    letterSpacing: -0.24,
+    lineHeight: 28,
+  },
+  subHead: {
+    fontFamily: FONT_FAMILY,
+    fontSize: 16,
+    fontWeight: '600',
+    letterSpacing: -0.16,
+    lineHeight: 24,
+  },
+  body: {
+    fontFamily: FONT_FAMILY,
+    fontSize: 16,
+    fontWeight: '400',
+    letterSpacing: -0.16,
+    lineHeight: 24,
+  },
+  label: {
+    fontFamily: FONT_FAMILY,
+    fontSize: 14,
+    fontWeight: '400',
+    letterSpacing: 0.14,
+    lineHeight: 22,
+  },
+  caption: {
+    fontFamily: FONT_FAMILY,
+    fontSize: 12,
+    fontWeight: '400',
+    letterSpacing: 0.12,
+    lineHeight: 16,
+  },
+});
