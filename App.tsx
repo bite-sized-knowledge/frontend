1fe0e1e6b170ce162f332eaec571502bda93c6b4
DaeHyun Kwon <104804antonio@gmail.com>
2025-01-25 21:31:56 +0900

    네비게이션 관련 패키지 설치

--- App.tsx
+++ App.tsx
@@ -12,12 +12,6 @@ import {TypoExample} from './src/screens/example/TypoExample';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
     <SafeAreaView style={backgroundStyle}>
       <TypoExample />
