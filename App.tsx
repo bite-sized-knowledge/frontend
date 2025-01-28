175e00e23da5c9d974403bf09443b1fe27646b76
DaeHyun Kwon <104804antonio@gmail.com>
2025-01-28 12:52:40 +0900

    style: 주석, import 정리

--- App.tsx
+++ App.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {BTab} from './src/navigator/BTab';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import 'react-native-gesture-handler';
-import {BNavigation} from './src/navigator/BNavigation';
 
 function App(): React.JSX.Element {
-  const backgroundStyle = {
-    flex: 1,
-    backgroundColor: 'blue',
-  };
-
   return (
-    // <SafeAreaView style={backgroundStyle}>
     <SafeAreaProvider>
       <StatusBar />
       <NavigationContainer>
         <BTab />
-        {/* <BNavigation /> */}
       </NavigationContainer>
     </SafeAreaProvider>
-    // </SafeAreaView>
   );
 }
 
