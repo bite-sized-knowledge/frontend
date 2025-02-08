35840dc598df20ced81b7a57a5aec182c904130a
DaeHyun Kwon <104804antonio@gmail.com>
2025-02-08 19:44:47 +0900

    fix: import 경로 간소화 추가 작업

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,36 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['module:react-native-dotenv', 'react-native-reanimated/plugin'], // 추가
+  plugins: [
+    'module:react-native-dotenv',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          '@assets': './src/assets',
+          '@api': './src/api',
+          '@components': './src/components',
+          '@context': './src/context',
+          '@hooks': './src/hooks',
+          '@navigator': './src/navigator',
+          '@screens': './src/screens',
+          '@styles': './src/styles',
+          '@types': './src/types',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ], // 추가
 };
