184d20544255be1514e895e1702ce2c0e9de6509
DaeHyun Kwon <104804antonio@gmail.com>
2025-03-10 22:40:33 +0900

    feat: 앱 진입시 자동로그인 판단 로직 추가

--- src/navigator/BStack.tsx
+++ src/navigator/BStack.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {useTheme} from '@/context/ThemeContext.tsx';
 import {BTab} from '@/navigator/BTab.tsx';
 import {Login} from '@/screens/Login/index.tsx';
 import {SignUp} from '@/screens/SignUp/index.tsx';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {refreshAccessToken} from '@/api/authApi';
 
 const Stack = createStackNavigator();
 
 export const BStack = () => {
   const insets = useSafeAreaInsets();
   const {theme} = useTheme();
+  const [initialRouteName, setInitialRouteName] = useState('login');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const checkToken = async () => {
+      try {
+        const accessToken = await AsyncStorage.getItem('accessToken');
+        const refreshToken = await AsyncStorage.getItem('refreshToken');
+        if (accessToken && refreshToken) {
+          await refreshAccessToken();
+        }
+
+        setInitialRouteName('tabNav');
+      } catch (e) {
+        setInitialRouteName('login');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  if (isLoading) return <></>;
 
   return (
     <Stack.Navigator
-      initialRouteName="login"
+      // initialRouteName={initialRouteName}
+      initialRouteName={'login'}
       screenOptions={{
         headerStyle: {
           height: 56 + insets.top,
