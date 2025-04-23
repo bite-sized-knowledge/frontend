83c8d582922d4ec75d4b835a2ff5cd8ee9c2143e
DaeHyun Kwon <104804antonio@gmail.com>
2025-04-23 21:24:13 +0900

    Update interestApi.ts

--- src/api/interestApi.ts
+++ src/api/interestApi.ts
@@ -9,16 +9,21 @@ interface Interest {
 }
 
 export const getInterests = async () => {
-  const {data} = await api.get<Interest[]>('/v1/meta/interests');
+  const {data} = await api.get<Interest[]>('/v1/meta/interests', {}, false);
 
   return data;
 };
 
 export const getGuestAccount = async (interestIds: number[]) => {
   try {
-    const {data} = await api.post<IToken>('/v1/members', {
-      interestIds,
-    });
+    const {data} = await api.post<IToken>(
+      '/v1/members',
+      {
+        interestIds,
+      },
+      {},
+      false,
+    );
 
     if (!data) return false;
 
