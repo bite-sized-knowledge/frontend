61a058b0ff39bd0d7ba4e8203acf4bf9512292d8
DaeHyun Kwon <104804antonio@gmail.com>
2025-04-23 21:29:19 +0900

    fix: 프로젝트 명 수정

--- ios/BITETests/BITETests.m
+++ ios/BITETests/BITETests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TecurationTests : XCTestCase
+@interface BITETests : XCTestCase
 
 @end
 
-@implementation TecurationTests
+@implementation BITETests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
